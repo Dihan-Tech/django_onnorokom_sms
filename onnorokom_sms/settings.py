18832bda4909d58e2d92ef15c0a965cf39bd5d26
Pritom Borogoria <pritom.saan@gmail.com>
2020-08-18 16:17:41 +0600

    SECRET_KEY moved to envrionemt variable

--- onnorokom_sms/settings.py
+++ onnorokom_sms/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q^ve)hbdmo(onmflr64psxj_--uxh^6xo-je&4n$s26_2m)wan'
+SECRET_KEY = os.getenv('SECRET_KEY', 'Token Not found')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
