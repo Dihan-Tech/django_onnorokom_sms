9caae6b5192a30fda09bdcf11d8ac60f14e6dbea
Pritom Borogoria <pritom.saan@gmail.com>
2020-08-20 16:33:11 +0600

    Model created with custom methods TestCases

--- django_onnorokom_sms/models.py
+++ django_onnorokom_sms/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.timesince import timesince as djtimesince
+from django.utils.timezone import now
+
 
 # Create your models here.
+class DjangoOnnorokomSMSModel(models.Model):
+    """This class holds the data of each sent email. message_text is for the text details of the message,
+       sent_by is an instance of the Auth User Model of the application. created_at holds the time
+       when the message has sent. sent_to is a text which helds the data of a signle of multiple receipts
+       number, mask_name and campaign_name holds the corresponding data if nothing used then it will be
+       null. sms_purpose is the topics on which that SMS has sent. It is a text field. If not sent then it
+       will be null. Only is_active True messages will be shown on List. status holds the response of the
+       OnnorokomSMS APIs."""
+    message_text = models.TextField(verbose_name='Message Text', null=True, blank=True)
+    sent_by = models.ForeignKey(get_user_model(), related_name='django_onnorokom_sms_sender',
+                                on_delete=models.CASCADE, db_index=True, verbose_name='SMS Sent By')
+    sent_to = models.TextField(verbose_name='Receipents', null=False, blank=False)
+    sms_purpose = models.TextField(verbose_name='SMS Purpose', null=True, blank=True)
+    mask_name = models.CharField(max_length=255, db_index=True, null=True, blank=True, verbose_name='Mask Name')
+    campaign_name = models.CharField(max_length=255, db_index=True, null=True, blank=True, verbose_name='Campaign Name')
+    is_success = models.BooleanField(verbose_name='Status', default=False)
+    is_active = models.BooleanField(verbose_name='Active', default=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+
+    class Meta:
+        verbose_name = 'Django OnnorokomSMS'
+        verbose_name_plural = 'Django OnnorokomSMSs'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        """Returns the first 10 characters of message body detail if it is greater than 10 if less and return
+           the whole message body detail as text"""
+        if len(self.message_text) >= 10:
+            return self.message_text[:10]
+        else:
+            return self.message_text
+
+    def get_timesince(self):
+        """This method returns the time difference between today and the SMS sent time"""
+        return djtimesince(self.created_at, now()).encode('utf8').replace(b'\xc2\xa0', b' ').decode('utf8')
---
+++ django_onnorokom_sms/tests/test_model_methods.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+from django_onnorokom_sms.models import DjangoOnnorokomSMSModel
+
+
+class DjangoOnnorokomSMSModelTestCases(TestCase):
+    model = DjangoOnnorokomSMSModel
+
+    def setUp(self):
+        test_user_one = get_user_model().objects.create(username='test_user_one', email='test_user_one@example.com', password='superacces123one')
+        self.model.objects.create(message_text='Greater than 10 characters', sent_by=test_user_one, sent_to='01XXXXXXXXX')
+        self.model.objects.create(message_text='Less', sent_by=test_user_one, sent_to='01XXXXXXXXX')
+
+    def test_model_str_method(self):
+        self.assertEqual(str(get_object_or_404(self.model, id=1)), 'Greater th')
+        self.assertEqual(str(get_object_or_404(self.model, id=2)), 'Less')
